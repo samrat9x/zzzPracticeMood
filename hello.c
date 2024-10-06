0f44efbdc0f2a00918a3e78ca8537d9e5ef10993
samrat9xm <primeminister9xm@gmail.com>
2024-10-06 10:05:49 +0600

    Practicing C

--- hello.c
+++ hello.c
@@ -9,7 +9,7 @@ int main(){
     
     float var3 = 4.0/9.0;
     printf("%0.2f\n", var3);
-    // by placing 0 after decimal point makes the integers double value. by default they are 'double constant'. if you want to make them float, add the suffix 'f'
+    // by placing numbers after decimal point makes the integers double. So by default they are 'double constant'. if you want to make them float, add the suffix 'f'
 
     return 0;
 }
