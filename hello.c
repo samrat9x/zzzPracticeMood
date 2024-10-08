3da744bae2eaab541f7959207dc5b135d575d5eb
samrat9xm <primeminister9xm@gmail.com>
2024-10-09 00:18:08 +0600

    fea

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 void main(){
-    printf("%10s","samrat");
+    printf("%2s","samrat");
 }
 
 // This %10s format specifier will prepend some blanks to "samrat" which will make it 10 characters in total
