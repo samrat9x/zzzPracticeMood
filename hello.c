5cc467417ee7dc807aef6ba0a3786d930045a13a
samrat9xm <primeminister9xm@gmail.com>
2024-10-09 13:31:49 +0600

    fe

--- hello.c
+++ hello.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
 
 void main(){
-    signed int i;
-    signed j;
-    unsigned k;
-    long l;
-    long int m;
-    long long n;
-    printf("%d,%d,%d,%d,%d,%d",i,j,k,l,m,n);
+    unsigned i = 1;
+    int j = -4;
+    printf("%u",i+j);// 4294967293
 }
+
+// The answer is -3 but as we use unsigned format specifier the 
