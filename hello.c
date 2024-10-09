e2e04ee489ffe52c70202b6544aff24a031c926d
samrat9xm <primeminister9xm@gmail.com>
2024-10-09 10:05:13 +0600

    no

--- hello.c
+++ hello.c
@@ -2,8 +2,9 @@
 
 void main(){
     char c = 255;
+    printf("%d\n",c);
     c = c + 10;
     printf("%d",c);
 }
 
-// This %10s format specifier will prepend some blanks to "samrat" which will make it 10 characters in total
+// Here c is signed character and a character typically can consume 8bits. So it overflows the range and 255 is equivalent to -1 (using 2's complement). So c is -1.
