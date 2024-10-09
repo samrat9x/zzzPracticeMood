19bf140d0b4688ace5bbaeef9fc484f9a0553290
samrat9xm <primeminister9xm@gmail.com>
2024-10-10 00:25:14 +0600

    adding two distinct data typed numbers

--- hello.c
+++ hello.c
@@ -6,4 +6,6 @@ void main(){
     printf("%u",i+j);// 4294967293
 }
 
-// The answer is -3 but as we use unsigned format specifier the 
+/* When you add 1 (unsigned) and -4 (signed), the result is -3. Since the output is printed using the unsigned format specifier (%u), -3 is interpreted as its unsigned 32-bit equivalent, which is 4294967293 due to two's complement representation.
+In two's complement, negative numbers are stored as large unsigned values. For a 32-bit system, -3 is represented as 11111111 11111111 11111111 11111101 in binary, which equals 4294967293 when treated as an unsigned integer. Hence, 4294967293 is printed.
+*/
