2dcd7e4e99380464bda7431acff8b28b267dc7cc
samrat9xm <primeminister9xm@gmail.com>
2024-10-21 22:25:59 +0600

    fe

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function $(e) {
-  if (e.includes("$") && e.includes(".")) {
-    return document.querySelectorAll(e);
-  }
-  if (e.includes(".")) {
-    return document.getElementsByClassName(e);
-  }
-  if (e.includes("#")) {
-    return document.getElementsById(e);
-  } else {
-    return document.querySelector(e);
-  }
-}
+console.log(NaN === NaN); // false
