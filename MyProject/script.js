dea9e1b83ddbca9eff27e90246aefbd31f89f6cf
samrat9x <primeminister9xm@gmail.com>
2024-12-19 22:06:39 +0600

    fd

--- MyProject/script.js
+++ MyProject/script.js
@@ -1,6 +1,14 @@
+const http = require('http');
 const fs = require('fs');
-const ourReadStream = fs.createReadStream(`${__dirname}/randomtext.txt`);
 
-ourReadStream.on('data', (chunk) => {
-    console.log(chunk.toString());
+const server = http.createServer((req, res) => {
+    const myReadStream = fs.createReadStream(
+        `${__dirname}/randomtext.txt`,
+        'utf8',
+    );
+    myReadStream.pipe(res);
+});
+
+server.listen(3000, (e) => {
+    console.log('Server is ready and listening on port 3000');
 });
